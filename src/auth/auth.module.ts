cf7dd8aec557ac90e97dc8d0f6180a62a60665a6
Camilo Duran <camilod529@gmail.com>
2025-05-20 19:52:14 -0500

    feat(auth): enhance registration process with JWT token generation and role handling

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { env } from 'src/config/env';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,31 +1,42 @@
 import { ForbiddenException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcryptjs from 'bcryptjs';
 import { PrismaClientKnownRequestError } from '../../generated/prisma/runtime/library';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { UserEntity } from './entities/user.entity';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   private readonly logger = new Logger(AuthService.name);
 
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async register(data: RegisterUserDto): Promise<Partial<UserEntity>> {
+  async register(data: RegisterUserDto) {
     try {
       const hashedPassword = await bcryptjs.hash(data.password, 10);
       const user = await this.prisma.user.create({
         data: {
-          email: data.email,
+          ...data,
           password: hashedPassword,
-          username: data.username,
         },
       });
 
+      const returnUser = new UserEntity({ ...user });
+
+      const token = this.generateToken({
+        id: returnUser.id,
+        email: returnUser.email,
+        roles: returnUser.role,
+      });
+
       return {
-        id: user.id,
-        email: user.email,
-        username: user.username,
+        ...returnUser,
+        token,
       };
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError && error.code === 'P2002') {
@@ -35,5 +46,9 @@ export class AuthService {
       this.logger.error('Error creating user', error);
       throw new InternalServerErrorException('Error creating user');
     }
+  }
+
+  private generateToken(payload: JwtPayload): string {
+    return this.jwtService.sign(payload);
   }
 }
